9a0f393e26c795f3530ec6fcc83fceeab65b0380
ABN <abn191@gmail.com>
2018-02-11 19:59:20 +0300

    update 5

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const os = require('os')
 const bot = new TelegramBot('496635681:AAERY7MoU5-dyYdZimuE78Bh3ritduHbnIg', {polling: true})
 
 bot.on('message', msg => {
-	bot.sendMessage(msg.chat.id, 'UPDATE 4')
+	bot.sendMessage(msg.chat.id, 'UPDATE 5')
 });
